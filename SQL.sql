8429ae1c87623a65f44f6de8afaff55020a565d2
Radasheva <eha20082@gmail.com>
2025-02-18 13:19:36 +0200

    create table om kirjeldatud

--- SQL.sql
+++ SQL.sql
@@ -11,48 +11,48 @@ CREATE TABLE - создать таблицу
 table_name - имя новой таблицы
 (column_name1 datatype, ...) - имя столбца и тип данных
 
-INSERT INTO table_name(column_name1, ...) 
+INSERT INTO table_name(column_name1, ...)  
 VALUES (column_value1, ...)
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - добавить в
+table_name - имя таблицы
+(column_name1 datatype, ...) - в какие столбцы вставить
+VALUES (column_value1, ...) - какие данные вставить
 
-DROP TABLE table_name
+DROP TABLE table_name - удалить таблицу
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - название таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - обновить данные
+table_name - название таблицы
+SET -  установить
+column_name1=column_value1 - что и куда 
+WHERE - где
+condition - условие 
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - изменить таблицу
+table_name - название таблицы
+ADD - добавить
+column_name2 datatype -  имя нового столбца + тип данных
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
+ALTER TABLE -изменить таблицу
+table_name - название таблицы
+ADD - добавить
+FOREIGN KEY -  первичный ключ
+(column_name) -
 References - 
 table_name2 - 
 (column_name2) - 
