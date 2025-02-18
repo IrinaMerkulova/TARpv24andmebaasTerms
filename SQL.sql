db697316d70ccc6b4e6bee1d8cafe884051e20a9
Nikita Gontsarov <nikitosgoldboss@gmail.com>
2025-02-18 13:29:13 +0200

    Create table on kirjutatud

--- SQL.sql
+++ SQL.sql
@@ -14,45 +14,45 @@ table_name - Имя столбца
 INSERT INTO table_name(column_name1, ...)
 VALUES (column_value1, ...)
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - Добавление новых данных в таблицу
+table_name - Название таблицы
+(column_name1 datatype, ...) - Определение структуры таблицы
+VALUES (column_value1, ...) - Вставка значений в таблицу
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - Удаление таблицы
+table_name - Имя таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - Изменение внесенных данных
+table_name - Имя таблицы
+SET - Обновление значений столбцов
+column_name1=column_value1 - Присвоение значения столбцу
+WHERE - Ограничения выборки данных (Равенство, Диапазон)
+condition - Условие
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - Изменение структуры существующей таблицы
+table_name - Имя таблицы
+ADD - Добавить
+column_name2 datatype - Изменение типа данных для нового столбца
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
-References - 
-table_name2 - 
-(column_name2) - 
+ALTER TABLE - Изменение структуры существующей таблицы
+table_name - Имя таблицы
+ADD - Добавить
+FOREIGN KEY - Вторичный ключ
+(column_name) - Имя столбца
+References - Создание внешних ключей (связь между таблицами)(Отсылка)
+table_name2 - Имя второй таблицы
+(column_name2) - Имя второго столбца
