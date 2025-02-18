2dc12b1a4b663272e1a73070288df13887c35d62
jelenakovaljova <jel.kovaljova@gmail.com>
2025-02-18 13:44:32 +0200

    sql commands

--- SQL.sql
+++ SQL.sql
@@ -7,34 +7,34 @@ table_name - Название таблицы
 
 CREATE TABLE (column_name1 datatype, ...)
 -----------------
-CREATE TABLE - 
-table_name - 
-(column_name1 datatype, ...) - 
+CREATE TABLE - создать таблицу
+table_name - название таблицы
+(column_name1 datatype, ...) - имя и тип данных
 
-INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+INSERT INTO table_name(column_name1, ...)вставить в таблицу
+VALUES (column_value1, ...) значения
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - вставить в
+table_name - имя таблицы
+(column_name1 datatype, ...) - имя и тип данных
+VALUES (column_value1, ...) - значения
 
-DROP TABLE table_name
+DROP TABLE table_name 
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - имя таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
+UPDATE - обновить
+table_name - имя таблицы
 SET - 
 column_name1=column_value1 - 
-WHERE - 
-condition - 
+WHERE - где
+condition - Условие запроса
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
