dc3ca0ea6808e965e15869ce054f47f2f38736c4
Eldar Aliev <eldar040503@gmail.com>
2025-02-18 13:22:48 +0200

    с 10 по 37 строки написаны

--- SQL.sql
+++ SQL.sql
@@ -11,30 +11,30 @@ CREATE TABLE - создать таблицу
 table_name - имя новой таблицы
 (column_name1 datatype, ...) - имя столбца и тип данных
 
-INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+INSERT INTO table_name(column_name1, ...) - внести в таблиу (название)
+VALUES (column_value1, ...) - значения для таблицы
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - вставить в
+table_name - название таблицы
+(column_name1 datatype, ...) - тип данных
+VALUES (column_value1, ...) - значения
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - сбросить таблцу
+table_name - название таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - внести изминения в таблицу
+table_name - название таблицы
+SET - заменить на
+column_name1=column_value1 - что на что через знак равно 
+WHERE - где
+condition - условие
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
