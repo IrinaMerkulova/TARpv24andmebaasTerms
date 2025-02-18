4902faec7ba97e9e1ae3af4c7fea05a3bf856dc7
artjompoldsaar <artjompoldsaar@gmail.com>
2025-02-18 13:18:14 +0200

    kõik päringud on kirjeldatud

--- SQL.sql
+++ SQL.sql
@@ -16,43 +16,43 @@ VALUES (column_value1, ...)
 -----------------
 INSERT INTO - Вставить в
 table_name - Название таблицы
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+(column_name1 datatype, ...) - Название столбца и его тип данных
+VALUES (column_value1, ...) - Значения (вводимые данные)
 
 DROP TABLE table_name
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - Сбросить таблицу
+table_name - Название таблицу
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - Обновить
+table_name - Название таблицу
+SET - Вставить значение
+column_name1=column_value1 - Название столбца = значение для столбца
+WHERE - Где
+condition - Условие
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
-ALTER TABLE - 
-table_name - 
-ADD - 
-column_name2 datatype - 
+ALTER TABLE - Изменить таблицу
+table_name - Название таблицы
+ADD - Добавить
+column_name2 datatype - Название столбца и его тип данных
 
 -------------------------------
 
 ALTER TABLE table_name 
 ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
-ALTER TABLE -
-table_name - 
-ADD - 
-FOREIGN KEY - 
-(column_name) - 
-References - 
-table_name2 - 
-(column_name2) - 
+ALTER TABLE - Изменить таблицу
+table_name - Название таблицы
+ADD - Добавить
+FOREIGN KEY - Вторичный ключ
+(column_name) - Название столбца
+References - Связь или ссылка
+table_name2 - Название другой таблицы
+(column_name2) - Название стобца из другой таблицы
