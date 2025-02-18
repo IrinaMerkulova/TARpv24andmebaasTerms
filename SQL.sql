26385893e6358353c8b310cee649e97abb79c294
Romanzaitsev <ihavearm0@gmail.com>
2025-02-18 13:19:34 +0200

    update table on kirjeldatud

--- SQL.sql
+++ SQL.sql
@@ -19,22 +19,22 @@ table_name - имя таблицы
 (column_name1 datatype, ...) - имя таблицы, тип данных
 VALUES (column_value1, ...) - внести данные в столбец
 
-DROP TABLE table_name
+DROP TABLE table_name - удалить таблицу
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - название таблицы
 
 ------------------------------------
 
-UPDATE table_name SET column_name1=column_value1
-WHERE condition
+UPDATE table_name SET column_name1=column_value1 - обновить имя таблицы  - имя таблицы - значение таблицы
+WHERE condition - вернёт все записи из таблицы где есть condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - обновить данные
+table_name - название таблицы
+SET - установить
+column_name1=column_value1 - изменение название с одного на другое
+WHERE - фильтрация записей
+condition - значение 
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
