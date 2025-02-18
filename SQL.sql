40ad7621eb0aaa10c56ae7c1a5c54f8bad0e7508
mareklukk <mareklukk8@gmail.com>
2025-02-18 14:18:42 +0200

    Põhimõisted on tõlgitud

--- SQL.sql
+++ SQL.sql
@@ -5,38 +5,38 @@ SELECT - Выбрать
 FROM - Из
 table_name - Название таблицы
 
-CREATE TABLE (column_name1 datatype, ...)
+CREATE TABLE (column_name1 datatype, ...) 
 -----------------
 CREATE TABLE - создать таблицу
 table_name - имя новой таблицы
 (column_name1 datatype, ...) - имя столбца и тип данных
 
-INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+INSERT INTO table_name(column_name1, ...) 
+VALUES (column_value1, ...) вставить значения 
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - вставить в
+table_name - название таблицы
+(column_name1 datatype, ...) - названия столбцов и тип данных
+VALUES (column_value1, ...) - значения для вставки
 
-DROP TABLE table_name
+DROP TABLE table_name -
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - удалить таблицу
+table_name - название таблицы
 
 ------------------------------------
 
 UPDATE table_name SET column_name1=column_value1
-WHERE condition
+WHERE condition 
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - обновить
+table_name -  название таблицы
+SET - установить
+column_name1=column_value1 - имя столбцо и его новое значение
+WHERE - где(условие)
+condition - заключение
 
-ALTER TABLE table_name ADD column_name datatype
+ALTER TABLE table_name ADD column_name datatype -
 -----------------
 ALTER TABLE - 
 table_name - 
@@ -45,8 +45,8 @@ column_name2 datatype -
 
 -------------------------------
 
-ALTER TABLE table_name 
-ADD FOREIGN KEY (column_name) References table_name2(column_name2)
+ALTER TABLE table_name - 
+ADD FOREIGN KEY (column_name) References table_name2(column_name2) -
 -----------------
 ALTER TABLE -
 table_name - 
