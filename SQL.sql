382994f3f7544b24f3669a21428f00d855711fa0
nikita.orlenko <nikita.orlenko@gmail.com>
2025-02-18 13:30:22 +0200

    Update table

--- SQL.sql
+++ SQL.sql
@@ -16,8 +16,8 @@ VALUES (column_value1, ...)
 -----------------
 INSERT INTO - вставка данных
 table_name - имя таблицы
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+(column_name1 datatype, ...) - список столбцов и их типов данных
+VALUES (column_value1, ...) -  указание значений, которые должны быть вставлены в таблицу в соответствующие столбцы
 
 DROP TABLE table_name
 -----------------
@@ -29,12 +29,12 @@ table_name - имя таблицы
 UPDATE table_name SET column_name1=column_value1
 WHERE condition
 -----------------
-UPDATE - 
-table_name - 
-SET - 
-column_name1=column_value1 - 
-WHERE - 
-condition - 
+UPDATE - обновлениe существующих данных в таблице
+table_name - имя таблицы
+SET -  для задания новых значений столбцов, которые нужно обновить
+column_name1=column_value1 - устанавливается новое значение
+WHERE - для указания условия
+condition - для фильтрации данных
 
 ALTER TABLE table_name ADD column_name datatype
 -----------------
@@ -50,8 +50,8 @@ ADD FOREIGN KEY (column_name) References table_name2(column_name2)
 -----------------
 ALTER TABLE -
 table_name - 
-ADD - 
-FOREIGN KEY - 
+ADD - добавить
+FOREIGN KEY - вторичный ключ
 (column_name) - 
 References - 
 table_name2 - 
