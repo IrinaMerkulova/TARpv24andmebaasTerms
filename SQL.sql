45dc4f989ee29e94cc3c6e5554ea55dddd1295a5
AdrianaPikaljov <pikaljovadri@gmail.com>
2025-02-18 13:15:18 +0200

    insert into ja drop table on kirjeldatud

--- SQL.sql
+++ SQL.sql
@@ -11,18 +11,18 @@ CREATE TABLE - tabeli loomine
 table_name - tabeli nimi
 (column_name1 datatype, ...) - veerude nimed ja andmetüübid
 
-INSERT INTO table_name(column_name1, ...)
-VALUES (column_value1, ...)
+INSERT INTO table_name(column_name1, ...) - lisab tabelise veerud
+VALUES (column_value1, ...) - sisestatakse veerudesse andmed
 -----------------
-INSERT INTO - 
-table_name - 
-(column_name1 datatype, ...) - 
-VALUES (column_value1, ...) - 
+INSERT INTO - lisada
+table_name - tabeli nimi
+(column_name1 datatype, ...) - veerud
+VALUES (column_value1, ...) - andmed
 
-DROP TABLE table_name
+DROP TABLE table_name - kustutab tabeli "table_name"
 -----------------
-DROP TABLE - 
-table_name - 
+DROP TABLE - kustuta tabel
+table_name - tabeli nimi
 
 ------------------------------------
 
